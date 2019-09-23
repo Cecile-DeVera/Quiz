53ab441fda15e2554aa4e454a438a59be797869c
Cecile DeVera <ceciledevera@Ceciles-MacBook-Pro.local>
2019-09-22 23:49:03 -0700

    modify scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,31 @@
 $("#recommendationQuiz").submit(function(event) {
+
   event.preventDefault();
-
   var answers = $(this).serializeArray();
+  var scores = {
+    python: 0,
+    javascript: 0,
+    csharp: 0,
+    ruby: 0,
+  };
+  for(var answer of answers){
+    scores[answer.value] += 1;
+  }
 
-  console.log(answers);
+  console.log(scores);
+  // //console.log(scores)
+  // for(var language in scores){
+  // //   console.log(language);
+  // }
+
+  var maxLanguage ="python"
+  for(var language in scores){
+    if(scores[language] > score[maxLanguage]){
+      maxLanguage = language;
+    }
+  }
+
+$("#recommendationQuiz").css('display', 'none');
+$(".result#"+maxLanguage).css('display', 'block');
 
 });
